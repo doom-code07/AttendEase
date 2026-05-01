9677ab6c09aa47858d9a0b708f8b90544914c6a2
seemab <ghayaassmuhammad@gmail.ocm>
2026-05-01 15:52:41 +0500

    database update url

--- src/main/java/HashTool.java
+++ src/main/java/HashTool.java
@@ -2,8 +2,8 @@ import dao.UserDAO;
 
 public class HashTool {
     public static void main(String[] args) {
-        String password = "viceprincipal123";
-        String pass = "admin123";
+        String password = "vicePrincipal@123";
+        String pass = "admiN@1234";
         String hashed = UserDAO.hashPassword(password);
         String hash = UserDAO.hashPassword(pass);
         System.out.println("Hashed Password: " + hashed);
--- src/main/java/utils/DBConnection.java
+++ src/main/java/utils/DBConnection.java
@@ -1,23 +1,26 @@
 package utils;
 
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "mysql://root:iCcljfZyLcxCEQhSZoWxXWoQhznvQGRa@gondola.proxy.rlwy.net:46612/railway";
-    private static final String USER = "root";
-    private static final String PASSWORD = "iCcljfZyLcxCEQhSZoWxXWoQhznvQGRa";
 
+    private static final String URL =
+            "jdbc:mysql://gondola.proxy.rlwy.net:46612/railway";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD =
+            "iCcljfZyLcxCEQhSZoWxXWoQhznvQGRa";
 
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("MySQL Driver not found", e);
         }
+
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
